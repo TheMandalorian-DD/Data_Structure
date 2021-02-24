cdecc5b9f58496a6f5803587c5da6d0aa75b8cc5
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-02-24 16:36:00 +0100

    modif

--- TME2/main.c
+++ TME2/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "entreeSortieLC.h"
 #include "biblioLC.h"
@@ -10,6 +11,16 @@ void menu(){
   puts("1.affichage biblio");
 
   puts("2.ajouter un ouvrage");
+
+  puts("3.recherche d’un ouvrage par son numero");
+
+  puts("4.recherche d’un ouvrage par son titre");
+
+  puts("5.recherche de tous les livres d’un meme auteur");
+
+  puts("6.suppression d’un ouvrage");
+
+  puts("7.recherche de tous les ouvrages avec plusieurs exemplaires");
 
   puts("press 0 to exit...");
 }
@@ -25,9 +36,19 @@ int main(int argc, char** argv){
 
     Biblio *b = charger_n_entrees(argv[1],atoi(argv[2]));
 
+    Biblio *bi = NULL;
+
+    Livre *livre;
+
     int rep;
 
     do{
+
+    	livre = NULL;
+
+    	printf("Que voulez-vous faire ? ...\n");
+
+    	sleep(1);
 
     	menu();
 
@@ -45,30 +66,120 @@ int main(int argc, char** argv){
 
     		case 2:
 
+    			printf("Veuillez entrer le numero, le titre et l'auteur de l'ouvrage.\n");
+
+    			/* On suppose que le titre et l’auteur ne contiennent pas d’espace*/
+
     			int num;
 
     			char titre[256];
 
     			char auteur[256];
 
-    			printf("Veuillez ecrire le numero, le titre et l'auteur de l'ouvrage.\n");
+    			if (scanf("%d %s %s",&num,titre,auteur)==3) {
+
+    				inserer_en_tete(b,num,titre,auteur);
+
+    				printf("Ajout fait.\n");
+
+    			} else {
+
+    				printf("Erreur format\n");
+
+    			}
+
+    			break;
+
+    		case 3:
+
+    			printf("Veuillez enntrer le numero de l'ouvrage.\n");
+
+    			if (scanf("%d",&num)==1){
+
+    				livre = recherche_ouvrage_num(b,num);
+
+    			}
+
+    			if (livre==NULL) printf("Ouvrage non trouvé\n");
+    			else printf("Ouvrage trouvé\n");
+
+    			afficher_livre(livre);
+
+    			break;
+
+    		case 4:
+
+    			printf("Veuillez enntrer le titre de l'ouvrage.\n");
+
+    			if (scanf("%s",titre)==1){
+
+    				livre = recherche_ouvrage_titre(b,titre);
+
+    			}
+
+    			if (livre==NULL) printf("Ouvrage non trouvé\n");
+    			else printf("Ouvrage trouvé\n");
+
+    			afficher_livre(livre);
+
+    			break;
+
+    		case 5:
+
+    			printf("Veuillez entrer le nom de l'auteur.\n");
+
+    			if (scanf("%s",auteur)==1){
+
+    				bi = recherche_meme_auteur(b,auteur);
+
+    			}
+
+    			if (bi->L==NULL) printf("Aucun ouvrage trouvé de cet auteur\n");
+    			else printf("Ouvrages trouvés\n");
+
+    			afficher_biblio(bi);
+
+    			liberer_biblio(bi);
+
+    			break;
+
+    		case 6:
+
+    			printf("Veuillez entrer le numero, le titre et l'auteur de l'ouvrage.\n");
 
     			/* On suppose que le titre et l’auteur ne contiennent pas d’espace*/
 
     			if (scanf("%d %s %s",&num,titre,auteur)==3) {
 
-    				inserer_en_tete(b,num,titre,auteur);
+    				b = supprimer_livre(b,num,titre,auteur);
 
-    				printf("Ajout fait.");
+    				printf("suppression fait.\n");
 
     			} else {
 
-    				printf("Erreur format");
+    				printf("Erreur format\n");
 
     			}
     			break;
+
+    		case 7:
+
+    			bi = recherche_exemplaires(b);
+
+    			if (bi->L==NULL) printf("Aucun exemplaires trouvés\n");
+    			else printf("Exemplaires trouvés\n");
+
+    			afficher_biblio(bi);
+
+    			liberer_biblio(bi);
+
+    			break;
+
+
     	}
     } while (rep != 0);
+
+    liberer_biblio(b);
 
     printf("Au revoir !\n");
 
