a9954578303700a18b7b4e78f9c36f9d50de2a21
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-03-03 14:40:54 +0100

    modif

--- TME2/biblioH.c
+++ TME2/biblioH.c
@@ -137,10 +137,8 @@ void afficher_biblioH(BiblioH* bh){
     if (bh==NULL) return;
 
     if (bh->nE == 0) return;
-
-    int i = 0;
     
-    for (LivreH* lh = bh -> T[i]; i < bh->m && lh; lh = bh -> T[i++]) afficher_livreH(lh);
+    for (int i = 0; i < bh->m; i++) afficher_livreH(bh->T[i]);
 
 }
 
